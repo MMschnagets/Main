474801839012d0668a029835f0a744b5f131e72c
MarkusMuz <125932795+MMschnagets@users.noreply.github.com>
2023-02-22 06:41:52 +0200

    top_3_words

--- codewars/top_3_words.py
+++ codewars/top_3_words.py
@@ -3,14 +3,23 @@ import re
 def top_3_words(text):
     exp = re.compile("[a-z']+", re.IGNORECASE | re.ASCII)
     words_list = exp.findall(text)
+    for x in words_list:
+        if (len(x) == x.count("'")):
+            words_list.remove(x)
     if len(words_list) == 0:
         return list()
     words_list = [x.lower() for x in words_list]
-    words_list = sorted(words_list, key = words_list.count, reverse = True)
+    count_list = [words_list.count(x) for x in words_list]
     top_3, k = list(), 0
     while (k < 3) and (len(words_list) > 0):
-        top_3.append(words_list[0]) 
-        words_list = words_list[0 + words_list.count(words_list[0]):]
+        max_count = max(count_list)
+        max_idx = count_list.index(max_count)
+        max_word = words_list[max_idx]
+        top_3.append(max_word)
+        for i in range(0, max_count):
+            tmp_idx = words_list.index(max_word)
+            words_list.pop(tmp_idx)
+            count_list.pop(tmp_idx)
         k+=1
 
     return top_3[:k]
