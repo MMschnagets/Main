9dcb812817ecd396b380787d3f9c9b6e9415ba5b
MarkusMuz <125932795+MMschnagets@users.noreply.github.com>
2023-02-22 04:42:36 +0200

    top_3_words

--- codewars/top_3_words.py
+++ codewars/top_3_words.py
@@ -1,6 +1,18 @@
 import re
 
 def top_3_words(text):
-    
-    
-    return ""
+    exp = re.compile("[a-z']+", re.IGNORECASE | re.ASCII)
+    words_list = exp.findall(text)
+    if len(words_list) == 0:
+        return list()
+    words_list = [x.lower() for x in words_list]
+    words_list = sorted(words_list, key = words_list.count, reverse = True)
+    top_3, k = list(), 0
+    while (k < 3) and (len(words_list) > 0):
+        top_3.append(words_list[0]) 
+        words_list = words_list[0 + words_list.count(words_list[0]):]
+        k+=1
+
+    return top_3[:k]
+
+top_3_words("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e")
