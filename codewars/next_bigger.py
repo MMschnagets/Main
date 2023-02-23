310c6e813485646f58350aa7c58c043352988623
MMschnagets <125932795+MMschnagets@users.noreply.github.com>
2023-02-23 14:55:03 +0200

    next_bigger

--- codewars/next_bigger.py
+++ codewars/next_bigger.py
@@ -1,2 +1,21 @@
 def next_bigger(n):
-    pass
+    if (n<10):
+        return -1
+    str_n = str(n)
+    digits_list = [int(x) for x in str_n]
+    digits_list.reverse()
+    length = len(digits_list)
+
+    for i in range(0, length):
+        for j in range(i + 1, length):
+            if (digits_list[i]>digits_list[j]):
+                digits_list[j], digits_list[i] = digits_list[i], digits_list[j]
+                str_num = str()
+                digits_list.reverse()
+                for k in digits_list:
+                    str_num += str(k)
+                return int(str_num)
+    
+    return -1
+
+next_bigger("144")
