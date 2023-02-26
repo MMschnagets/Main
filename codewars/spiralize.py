3d107f34e1199803d449737e59a1c8ec429f4901
MarkusMuz <125932795+MMschnagets@users.noreply.github.com>
2023-02-26 20:00:26 +0200

    spiralize

---
+++ codewars/spiralize.py
@@ -0,0 +1,7 @@
+def spiralize(size):
+    spiral = [[0 for i in range(0, size)] for j in range(0, size)]
+    for i in range(1, size-1):
+        line = list()
+        for j in range(0, size):
+            pass
+    return spiral
